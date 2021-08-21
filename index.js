443955cd2c9c2d154f0f1c53a4fca13c2b992306
zhuqinfei <1586859762@qq.com>
2021-08-21 16:18:07 +0800

    利用图片发起get请求实现

--- index.js
+++ index.js
@@ -28,13 +28,16 @@ var server = http.createServer(function(request, response){
     var string = fs.readFileSync('./main.js')
     response.setHeader('Content-Type', 'application/javascript')
     response.end(string)
-  }else if(path === '/pay' && method.toUpperCase() === 'POST'){
+  }else if(path === '/pay'){
     var amount = fs.readFileSync('./db','utf-8')  
     var newAmount=amount-1
      if(Math.random()>0.5){
        fs.writeFileSync('./db',newAmount)
-       response.write('success')
+       response.setHeader('Content-Type', 'image/jpg')
+       response.statusCode=200
+       response.write(fs.readFileSync('./dog.jpg'))
      }else{
+      response.statusCode=400
        response.write('fail')
      }
     response.end()   
